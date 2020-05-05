cb02633e681e784500665f50bc5f12d5b0da8bec
claudio <claudiomr79@gmail.com>
2020-05-05 20:17:56 -0300

    se agrego capa de datos para pruebas, ref entre los proyectos, metodos para ABMC, hay errores

---
+++ Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+
+namespace Business.Logic
+{
+    public class UsuarioLogic: BussinessLogic
+    {
+        public UsuarioAdapter UsuarioData { get; set; }
+
+        public UsuarioLogic()
+        {
+            this.UsuarioData = new UsuarioAdapter();
+        }
+        public List<Usuario> GetAll()
+        {
+            return this.UsuarioData.GetAll();
+        } 
+        public Usuario GetOne (int Id)
+        {
+            return this.UsuarioData.GetOne(Id);
+        }
+        public void Delete (int Id)
+        {
+            return this.UsuarioData.Delete(Id);
+        }
+        public void Save (Usuario us)
+        {
+            return this.UsuarioData.Save(us);
+        }
+    }
+}
